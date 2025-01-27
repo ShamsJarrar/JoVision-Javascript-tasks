24e30713c12d8e681471b0fe5c0c82858d09290b
Shams <shamsrami2004@gmail.com>
2025-01-27 15:17:34 +0400

    Modified task 4 code to include task 5, renamed the folder

--- Task 4 and 5/script.js
+++ Task 4 and 5/script.js
@@ -4,9 +4,14 @@ const text = document.querySelector("p");
 button.addEventListener("click", function(){
     const arr = [];
     let sum = 0;
+    let evenSum = 0;
     for(let i = 0; i <= 100; i++){
         arr.push(i);
         sum += i;
+        if(i % 2 == 0){
+            evenSum += i;
+        }
     }
-    text.innerHTML = "Sum = " + sum;
+    text.innerHTML = "Sum of all numbers = " + sum + 
+                    "<br> Sum of even numbers only = " + evenSum;
 })
